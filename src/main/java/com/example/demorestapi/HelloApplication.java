b0a4b3ab15ac19e6ebc9ee043a7a4643a2aa654d
Alestesa <56315507+Alestesa@users.noreply.github.com>
2022-01-09 21:35:03 +0100

    Update 1 - GUI

--- src/main/java/com/example/demorestapi/HelloApplication.java
+++ src/main/java/com/example/demorestapi/HelloApplication.java
@@ -1,23 +1,76 @@
 package com.example.demorestapi;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class HelloApplication extends Application {
+
+    public static final int WIDTH = 640, HEIGHT = 800;
+
+    private ArrayList<Image> img = new ArrayList<>();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        VBox root = fxmlLoader.load();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        init(root, scene);
+
+        stage.setTitle("iTasker Desktop");
+        stage.getIcons().add(new Image("file:res/ico.png"));
+        stage.setResizable(false);
         stage.setScene(scene);
+
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void init(VBox root, Scene scene) throws FileNotFoundException {
+        String[] img = {
+                "res/_login.png", "res/_login_2.png",
+                "res/bg.png", "res/ime.png"
+        };
+
+        for(String _img : img)
+            this.img.add(new Image(new FileInputStream(_img)));
+
+        ImageView ime = (ImageView) scene.lookup("#ime");
+        ime.setImage(this.img.get(3));
+
+        ImageView login = new ImageView(this.img.get(0));
+        login.setOnMouseEntered(mouseEvent -> { swapLoginImg(login); });
+        login.setOnMouseExited(mouseEvent -> { swapLoginImg(login); });
+
+        Pane loginPane = (Pane) scene.lookup("#loginPane");
+        /*loginPane.setBackground(new Background(new BackgroundImage(this.img.get(2),
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT)));*/
+
+        loginPane.getChildren().add(login);
+    }
+
+    private void swapLoginImg(ImageView login){
+        Image newImg = login.getImage() == this.img.get(0)?
+                this.img.get(1) : this.img.get(0);
+        login.setImage(newImg);
     }
 }
--- src/main/java/com/example/demorestapi/HelloController.java
+++ src/main/java/com/example/demorestapi/HelloController.java
@@ -1,11 +1,22 @@
 package com.example.demorestapi;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
+public class HelloController{
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private VBox parent;
+
+    @FXML
+    private PieChart pie;
+
 
     @FXML
     protected void onHelloButtonClick() {
@@ -14,5 +25,21 @@ public class HelloController {
     @FXML
     protected void onMybuttononClick() {
         welcomeText.setText("Pressed");
+    }
+
+    @FXML
+    protected void initialize(){
+        initPie();
+    }
+
+
+    private void initPie(){
+       /* ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Iphone 5S", 13),
+                new PieChart.Data("Samsung Grand", 25),
+                new PieChart.Data("MOTO G", 10),
+                new PieChart.Data("Nokia Lumia", 22));
+
+        pie.setData(pieChartData);*/
     }
 }
